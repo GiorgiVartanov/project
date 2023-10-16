cfcd7052c1d678a9b7195d078f83b4e03d1bd8f6
PhYell <94902895+PhYell@users.noreply.github.com>
2023-10-16 21:29:39 +0400

    changed path to client

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")))
 
   app.get("*", (req: Request, res: Response) =>
-    res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../", "client", "dist", "index.html"))
   )
 } else {
   app.get("/", (req: Request, res: Response) => res.send("you need to set to production"))
